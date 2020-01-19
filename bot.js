87639981c8a8aa5531824584d259a809a6431c6c
SimplyHate <32654992+SimplyHate@users.noreply.github.com>
2020-01-18 21:28:24 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,8 @@
+const = Discord =  register('discord.js');
+const = client = new Discord.client();
+
+client.login("NjYyNTI0OTgxNzI0NDQ2NzU2.XiO7Kg.1kIodXdmPx0Z1iYlikB8kNuJmQk");
+
+client.on("ready" , () => {
+    console.log("The bot is online!");;
+}]
